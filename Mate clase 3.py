f630b951e2fcd553a926fcb18e5de1b16905e2bd
71762257 <71762257@certus.edu.pe>
2021-04-24 11:23:09 -0500

    Trabajo 24/04

--- Mate clase 3.py
+++ Mate clase 3.py
@@ -1,3 +1,17 @@
+#Concatenacion
+a="uno"
+b="dos"
+print(a+b)
+print(a*3)
+
+#
+a=23
+print("a=23 -->", a)
+a=a+1
+print("a=a+1 -->", a)
+a+=1
+print("a+=1 -->", a)
+a*=2
 print("a*=2 -->", a)
 a/=2
 print("a/=2 -->", a)
@@ -5,4 +19,4 @@ a-=2
 print("a-=1 -->", a)
 a**=2
 print("a**=1 -->", a)
-#decimales
+
